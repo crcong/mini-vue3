5239b010e6764bab0ac2b415bc39bac8990e3a2d
Ruicong <crcong@foxmail.com>
2022-05-15 11:59:29 +0800

    refactor(runtime-core): use ShapeFlags

--- src/runtime-core/src/renderer.ts
+++ src/runtime-core/src/renderer.ts
@@ -1,4 +1,4 @@
-import { isObject } from '../../shared'
+import { ShapeFlags } from '../../shared/ShapeFlags'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -6,9 +6,10 @@ export function render(vnode, container) {
 }
 
 function patch(vnode, container) {
-  if (typeof vnode.type === 'string') {
+  const { shapeFlag } = vnode
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container)
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container)
   }
 }
@@ -16,10 +17,10 @@ function patch(vnode, container) {
 function processElement(vnode, container) {
   const el = vnode.el = document.createElement(vnode.type)
 
-  const { children, props = {} } = vnode
-  if (typeof children === 'string') {
+  const { children, props = {}, shapeFlag } = vnode
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el)
   }
 
